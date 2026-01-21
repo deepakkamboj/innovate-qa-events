b04f00699b0bc8a55faa0c1e53e532f2f33336d6
Deepak Kamboj <deepakkamboj@gmail.com>
2026-01-20 20:26:45 -0800

    updating site

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/innovate-qa-events',
+  assetPrefix: '/innovate-qa-events',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-  // If your repo name is not the root domain, uncomment and set basePath
-  basePath: '/innovate-qa-events',
 }
 
 export default nextConfig
