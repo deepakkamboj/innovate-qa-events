96a0e2787fd0cff132de882840b546d61a51500e
Deepak Kamboj <deepakkamboj@gmail.com>
2026-01-20 20:44:02 -0800

    Remove custom image loader, add trailingSlash to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,12 @@ const nextConfig = {
   output: 'export',
   basePath: '/innovate-qa-events',
   assetPrefix: '/innovate-qa-events',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
-    loader: 'custom',
-    loaderFile: './next-image-loader.js',
   },
 }
 
